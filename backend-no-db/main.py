220a750eda57f18f182bbbce7d825a1343312cd4
William Chen <williamchen120@gmail.com>
2025-01-25 16:00:39 -0800

    RESTful MongoDB for Experience

--- backend-no-db/main.py
+++ backend-no-db/main.py
@@ -1,26 +1,72 @@
 from flask import Flask, request, jsonify
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from os import getenv
+from dotenv import load_dotenv
 
+
+load_dotenv()
 app = Flask(__name__)
+USER = getenv('USER')
+PASSWORD = getenv('PASSWORD')
+uri = (
+    f"mongodb+srv://{USER}:{PASSWORD}@capstone.fw3b6.mongodb.net/?"
+    "retryWrites=true&w=majority&appName=Capstone"
+)
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
 
 
-@app.route('/api/data', methods=['POST', 'GET'])
+@app.route('/api/data/experience', methods=['POST', 'GET', 'DELETE', 'PUT'])
 def request_handler():
 
-    if request.method == 'POST':
-        data = request.get_json()
+    # Grabs collection DB
+    db_experience = client["Experience"]
+    collection_experience = db_experience["Experience"]
 
+    if request.method == 'POST':
+        # Add Data
+        data = request.get_json()['Test']
+        print(data)
+        # collection_experience.insert_one(jsonify(dummy_data))
         response = {
-            'message': 'Data received successfully',
-            'data': data
+            "Message": "Successfully Created"
         }
         return jsonify(response)
 
     if request.method == 'GET':
+        # Get Data
+        response_data = [data for data in collection_experience.find()]
+        _strip_id(response_data)
         response = {
-            'Request Method': 'POST',
-            'Message': 'Have a Jolly Good Day'
+            'Message': "Successfully Retrieved",
+            'data': response_data
         }
         return jsonify(response)
+
+    if request.method == 'Delete':
+        # Update Data
+        collection_experience.delete_one({'Experience': 'Bahamas'})
+        response = {
+            "Successfully Updated"
+        }
+        return jsonify(response)
+
+    if request.method == 'Update':
+        collection_experience.update_one(
+            {'Experience': 'Bahamas'},
+            {"$set": {'Experience': 'Hawaii'}}
+        )
+        response = {
+            "Successfully Updated"
+        }
+        return jsonify(response)
+
+
+def _strip_id(input_data: object) -> None:
+    for data in input_data:
+        del data['_id']
 
 
 if __name__ == '__main__':
