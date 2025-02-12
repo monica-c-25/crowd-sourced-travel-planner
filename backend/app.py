8a1613bd55a66ce044682caf3835acaa06e89c86
Sidney Wong <wongsid@oregonstate.edu>
2025-02-11 23:53:45 -0800

    Moved photo handler from app.py to main.py.
    Photos uploaded will continued to be stored in existing bucket in Google Cloud Storage. A Photo Data dictionary containing unique file name (key) and photo URL (value) will be stored as a list within the Experiences collection. When a GET request is sent for images, it returns a response containing a list of all URL's under an experience.

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,8 @@ from flask_cors import CORS, cross_origin
 from authlib.integrations.flask_client import OAuth
 from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
-from google.cloud import storage, datastore
-import io
-import datetime
+# from google.cloud import storage, datastore
+# import io
 
 # Load environment variables
 ENV_FILE = find_dotenv()
@@ -96,145 +95,6 @@ def sync_user():
 
     return jsonify({"message": "User synced successfully"}), 200
 
-
-# ------------------- Image Storage -------------------
-datastore_client = datastore.Client()
-PHOTO_BUCKET = 'cs467-crowd-sourced-travel-planner-images'
-
-
-def _get_next_photo_id():
-    # Find the highest current photo ID in the collection
-    last_photo = photo_collection.find().sort("_id", -1).limit(1)
-    if last_photo.count() > 0:
-        # Increment the highest ID found
-        return last_photo[0]["_id"] + 1
-    else:
-        # If no photos exist, start with 1
-        return 1
-
-
-@app.route('/images', methods=['GET', 'POST'])
-def upload_image():
-    if request.method == 'GET':
-        # Render the file upload form when visiting /images
-        return render_template('file_upload.html')
-
-    if request.method == 'POST':
-        # Any files in the request will be available in request.files object
-        # Check if there is an entry in request.files with the key 'file'
-        if 'file' not in request.files:
-            return ('No file sent in request', 400)
-        # Set file_obj to the file sent in the request
-        file_obj = request.files['file']
-        # Create a storage client
-        storage_client = storage.Client()
-        # Get a handle on the bucket
-        bucket = storage_client.get_bucket(PHOTO_BUCKET)
-        # Create a blob object for the bucket with the name of the file
-        blob = bucket.blob(file_obj.filename)
-        # Position the file_obj to its beginning
-        file_obj.seek(0)
-        try:
-            photo_id = _get_next_photo_id()
-            # Upload the file into Cloud Storage
-            blob.upload_from_file(file_obj)
-            image_url = blob.public_url
-
-            # Insert photo metadata into Photo collection
-            photo_payload = {
-                "_id": photo_id,
-                "file_name": file_obj.filename,
-                "url": image_url,
-                "uploaded_at": datetime.datetime.utcnow()
-            }
-            photo_collection.insert_one(photo_payload)
-
-            return jsonify({'photo_id': photo_id, 'image_url': blob.public_url, 'file_name': file_obj.filename, 'uploaded_at': datetime.datetime.utcnow()}), 201
-
-        except Exception as e:
-            # Handle exceptions during file upload
-            return jsonify({'error': str(e)}), 500
-
-
-@app.route('/images/<file_name>', methods=['GET'])
-def get_image(file_name):
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(PHOTO_BUCKET)
-    # Create a blob with the given file name
-    blob = bucket.blob(file_name)
-    # Handle empty or missing files
-    if not blob.exists():
-        return jsonify({'error': "File not found"}), 404
-    # Create a file object in memory using Python io package
-    file_obj = io.BytesIO()
-    # Download the file from Cloud Storage to the file_obj variable
-    blob.download_to_file(file_obj)
-    # Position the file_obj to its beginning
-    file_obj.seek(0)
-    # Send the object as a file in the response with the correct MIME type and file name
-    image_url = blob.public_url
-    return jsonify({'image_url': image_url})
-    # return send_file(file_obj, mimetype=None, download_name=file_name)
-    # return image_url  #Returns the url of the image instead of download
-
-
-@app.route('/images/<file_name>', methods=['DELETE'])
-def delete_image(file_name):
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(PHOTO_BUCKET)
-    blob = bucket.blob(file_name)
-    # Check if file exists in Cloud Storage
-    if not blob.exists():
-        return jsonify({'error': "File not found"}), 404
-    # Delete the file from Cloud Storage
-    blob.delete()
-    # Delete corresponding photo metadata from MongoDB
-    result = photo_collection.delete_one({"file_name": file_name})
-    # Check if photo exists in MongoDB to begin with.
-    if result.deleted_count == 0:
-        return jsonify({'error': "Photo did not exist in MongoDB"}), 404
-    return jsonify({"message": "Image successfully deleted."}), 204
-
-@app.route('/images/experience/<experience_id>', methods=['GET'])
-def get_images_by_experience_id(experience_id):
-    photos = photo_collection.find({"experience_id": experience_id})
-    # Check if any photos exist under experience_id
-    if photos.count() == 0:
-        return jsonify({'error': "No images found for this experience ID"}), 404
-    
-    #Compiles a list of photo urls that fall under experience_id
-    image_urls = [photo['url'] for photo in photos]
-    return jsonify({'experience_id': experience_id, 'image_urls': image_urls}), 200
-
-
-@app.route('/images/experience/download/<experience_id>', methods=['GET'])
-def download_images_by_experience(experience_id):
-    photos = photo_collection.find({"experience_id": experience_id})
-    if photos.count() == 0:
-        return jsonify({'error': "No images found for this experience ID"}), 404
-    
-    images = []
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(PHOTO_BUCKET)
-
-    # Iterate over each image to download it from Google Cloud Storage
-    for photo in photos:
-        # Get the file name and create a blob
-        file_name = photo['file_name']
-        blob = bucket.blob(file_name)
-        
-        # Ensure the file exists in Cloud Storage
-        if blob.exists():
-            file_obj = io.BytesIO()  # Create an in-memory file object
-            blob.download_to_file(file_obj)  # Download the image into the in-memory object
-            file_obj.seek(0)  # Reset the file object position to the beginning
-            
-            # Append the image to the list
-            images.append({'file_name': file_name, 'file_obj': file_obj})
-        else:
-            return jsonify({'error': f"File {file_name} not found in storage"}), 404
-        # Returns first image only
-        return send_file(images[0]['file_obj'], mimetype='image/jpeg', download_name=images[0]['file_name'])
 
 if __name__ == '__main__':
     app.run(port=46725, debug=True)
--- backend/backend-MongoDB/main.py
+++ backend/backend-MongoDB/main.py
@@ -5,6 +5,8 @@ from os import getenv
 from dotenv import load_dotenv
 from HTTP_funcs import _get, _put, _post, _delete, _set_payload
 from flask_cors import CORS
+from google.cloud import storage, datastore
+from bson import ObjectId
 
 load_dotenv()
 app = Flask(__name__)
@@ -125,6 +127,159 @@ def user_request_handler():
                 'Message': f"Failed: {exception} raised"
             }
         return jsonify(response)
+
+
+# ------------------- Photo Storage -------------------
+datastore_client = datastore.Client()
+PHOTO_BUCKET = 'cs467-crowd-sourced-travel-planner-images'
+
+# def _get_next_photo_id():
+#     db = client["Photo"]
+#     collection = db["Photo"]
+#     # Find the highest current photo ID in the collection
+#     last_photo = collection.find().sort("_id", -1).limit(1)
+#     if last_photo.count() > 0:
+#         # Increment the highest ID found
+#         return last_photo[0]["_id"] + 1
+#     else:
+#         # If no photos exist, start with 1
+#         return 1
+
+
+@app.route('/api/photos', methods=['POST', 'GET', 'DELETE', 'PUT'])
+def photo_request_handler():
+    db = client["Experience"]
+    collection = db["Experience"]
+
+    if request.method == 'POST':
+        # Add Photo
+        try:
+            # Any files in the request will be available in request.files object
+            # Check if there is an entry in request.files with the key 'file'
+            if 'file' not in request.files:
+                return ('No file sent in request', 400)
+            # Set file_obj to the file sent in the request
+            file_obj = request.files['file']
+            # Create a storage client
+            storage_client = storage.Client()
+            # Get a handle on the bucket
+            bucket = storage_client.get_bucket(PHOTO_BUCKET)
+            file_name = f"{str(ObjectId())}_{file_obj.filename}"
+            # Create a blob object for the bucket with the name of the file
+            blob = bucket.blob(file_name)
+            # Position the file_obj to its beginning
+            file_obj.seek(0)
+
+            # # Generate new photo_id
+            # photo_id = _get_next_photo_id()
+
+            # Upload the file into Cloud Storage
+            blob.upload_from_file(file_obj)
+            photo_url = blob.public_url
+
+            photo_data = {
+                "file_name": file_name,
+                "photo_url": photo_url
+            }
+
+            # Save photo_url into Experience Collection
+            experience_id = request.form.get('experience_id')
+            # experience_id = request.args.get("experience_id")
+            # experience = collection.find_one({"_id" : experience_id})
+            experience = collection.find_one({"_id": ObjectId(experience_id)})
+            if experience:
+                collection.update_one(
+                    {"_id": experience_id},
+                    {"$push": {"photo_data": photo_data}}
+                )
+                response = {
+                    "Message": "Success",
+                    "ID": _post(collection, request.get_json())
+                }
+                return jsonify(response)
+            else:
+                return jsonify({'Error': "Experience Not Found"}), 404
+
+        except Exception as e:
+            return jsonify({"Error": str(e)}), 500
+
+    if request.method == 'GET':
+        # Sample GET Request: GET /api/photos?experience_id=some_experience_id
+        # Get Data
+        try:
+            experience_id = request.args.get("experience_id")
+            # experience_id = request.form.get('experience_id')
+            if not experience_id:
+                return jsonify({"Error": "Experience ID Required"}), 400
+
+            # experience = collection.find_one({"_id" : experience_id})
+            experience = collection.find_one({"_id": ObjectId(experience_id)})
+            if experience:
+                photo_data = experience.get("photo_data", [])
+                photo_urls = [photo["photo_url"] for photo in photo_data]
+
+                if not photo_urls:
+                    response = {
+                        "message": "No photos found for this experience",
+                        "experience_id": ObjectId(experience_id)
+                    }
+                    return jsonify(response)
+                response = {
+                    "message": "Success",
+                    "experience_id": experience_id,
+                    "photo_urls": photo_urls
+                }
+                return jsonify(response), 200
+            else:
+                return jsonify({'Error': "Experience Not Found"}), 404
+
+        except Exception as e:
+            return jsonify({"Error": str(e)}), 500
+
+    if request.method == 'DELETE':
+        # Sample DELETE Request: DELETE /api/photos?experience_id=some_experience_id&photo_url=https://storage.googleapis.com/cs467-crowd-sourced-travel-planner-images/photo1.jpg
+        # Update Data
+        try:
+            experience_id = request.args.get("experience_id")
+            # experience_id = request.form.get('experience_id')
+            photo_url_to_delete = request.args.get('photo_url')
+            if not experience_id or not photo_url_to_delete:
+                return jsonify({"Error": "Experience_id and Photo_url are required."}), 400
+
+            file_name = photo_url_to_delete.split('/')[-1]
+
+            storage_client = storage.Client()
+            bucket = storage_client.get_bucket(PHOTO_BUCKET)
+
+            blob = bucket.blob(file_name)
+            # Check if file exists in Cloud Storage
+            if not blob.exists():
+                return jsonify({'error': "File not found"}), 404
+            # Delete the file from Cloud Storage
+            blob.delete()
+            # Delete corresponding photo metadata from MongoDB
+            result = collection.delete_one({"file_name": file_name})
+
+            # experience = collection.find_one({"_id" : experience_id})
+            experience = collection.find_one({"_id": ObjectId(experience_id)})
+            if experience:
+                result = collection.update_one(
+                    {"_id": experience_id},
+                    {"$pull": {"photo_data": {"photo_url": photo_url_to_delete}}}
+                )
+
+                if result.modified_count > 0:
+                    response = {
+                        "message": "Success: Photo URL Removed",
+                        "experience_id": experience_id,
+                        "removed_photo_url": photo_url_to_delete
+                    }
+                    return jsonify(response), 200
+                else:
+                    return jsonify({'Error': "Experience Not Found"}), 404
+
+        except Exception as e:
+            return jsonify({"Error": str(e)}), 500
 
 
 if __name__ == '__main__':
